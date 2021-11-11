bd70fb607e995f749571f338f18745ce14199049
Dominik <dominikgzd@gmail.com>
2021-11-11 13:58:04 +0100

    Add AddressEntity and Repository

---
+++ models-service/src/main/java/pl/shop/bike/models/model/entities/address/AddressEntity.java
@@ -0,0 +1,30 @@
+package pl.shop.bike.models.model.entities.address;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String street;
+    private Integer houseNumber;
+    private Integer localNumber;
+    private String zipCode;
+    private String city;
+    private String phone;
+    private String email;
+}
